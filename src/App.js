7d857a11fe9fc402e7a89310fd18b0bc3b60c581
Anisha McDowell <anishamcdowell@gmail.com>
2021-08-20 22:41:51 -0400

    First font color/style set up for h1-3 and p

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Piece of Mind</h1>
+        <h2>The Mental Health Tracker</h2>
+        <h3>H3 test</h3>
+        <p>Paragraph test laksdjfaldsfjaowiefnawlk sldjka  ioalefnal fajefiowea fjalwfjal <br />fji fk aie aljf aiofja aleifja lfj foaifajlfja.</p>
       </header>
     </div>
   );
